f2d3115b23e2b0fbd38e1acfd0b08650d40bdb59
sunnychin <sunnychin@vip.163.com>
2018-07-28 07:03:54 +0800

    add agent

---
+++ Tower/Assets/Scripts/Network/Agent.cs
@@ -0,0 +1,21 @@
+﻿using DreamEngine.Net;
+using DreamEngine.Net.Protocols;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Agent : AgentBase
+{
+    protected override ProtocolBaseManager ProtocolManager
+    {
+        get
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public Agent(Socket s, int sendBufferSize = 1048576, int recvBufferSize = 1048576) : base(s, sendBufferSize, recvBufferSize)
+    {
+
+    }
+
+}
